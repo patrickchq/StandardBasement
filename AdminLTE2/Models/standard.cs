88d795f0c6bf3e565dc2005b4b8cce5f8bd78d7c
patrickchq <36976812+patrickchq@users.noreply.github.com>
2018-06-14 21:10:37 +0800

    添加项目文件。

---
+++ AdminLTE2/Models/standard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminLTE2.Models
+{
+    public class standard
+    {
+        public int standardId{get;set;}
+        public string standardPrefix { get; set; }
+        public string standardIdenttity { get; set; }
+        public string standardTitle { get; set; }
+        
+     }
+    public class ULStandard
+    {
+
+    }
+}
